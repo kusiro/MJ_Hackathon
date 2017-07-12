0308d7270deb01db603f02d9a69ab0001cf694b3
kusiro <jackysoon871008@gmail.com>
2017-07-12 20:55:05 +0800

    Fix react
    把 script 搬到下面去

--- assets/main.js
+++ assets/main.js
@@ -4,26 +4,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import sticky from "./js/sticky.js";
 
-// class Selection_b extends React.Component {
-//     render() {
-//         return(
-//             <div>
-//                 <div class="selection">Intro</div>
-//                 <div class="selection">Join us</div>
-//                 <div class="selection">Q & A</div>
-//                 <div class="selection">Partners</div>
-//                 <div class="selection">Team</div>
-//                 <div class="selection">Contact</div>
-//             </div>
-//         );
-//     }
-// }
-//
-// ReactDom.render( <Selection_b/>, document.getElementById("selectionBar"));
+class Selection_b extends React.Component {
+    render() {
+        return(
+            <div id="selectionBar">
+                <div class="selection">Intro</div>
+                <div class="selection">Join us</div>
+                <div class="selection">Q & A</div>
+                <div class="selection">Partners</div>
+                <div class="selection">Team</div>
+                <div class="selection">Contact</div>
+            </div>
+        );
+    }
+}
 
-
-const element = <h1>Hello, world</h1>;
-ReactDOM.render(
-  element,
-  document.getElementById('app')
-);
+ReactDOM.render( <Selection_b/>, document.getElementById("Bar"));
