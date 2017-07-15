7a076b2b56af7a52fc7884b21f21f5628f6334b6
kusiro <jackysoon871008@gmail.com>
2017-07-15 12:29:16 +0800

    writing HOW
    開始寫 報名流程

--- assets/js/parall.js
+++ assets/js/parall.js
@@ -8,3 +8,11 @@ $('#block_1').parallax({
     positionY: '0px',
     zpotision: '-100'
 });
+
+
+$('#block_2').parallax({
+    imageSrc: 'Img/pexels-photo-267507.jpg',
+    speed: '.5',
+    positionY: '0px',
+    zpotision: '-100'
+});
