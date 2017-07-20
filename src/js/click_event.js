100a564f4f6f3ce09cdef1e5e76a0f1a2204d876
kusiro <jackysoon871008@gmail.com>
2017-07-20 14:18:34 +0800

    issue
    click - scroll event

--- src/js/click_event.js
+++ src/js/click_event.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
+    var lock = false;
     $('.selection').click(function(){
-
+        if( lock == false){
+            lock = true;
+            $('html,body').animate({scrollTop: $("#intro").offset().top},1500);
+            lock = false;
+        }
     })
 });
