ca958515650d9856bb4b83b6f8651e38117ccfcf
Sunset <s2821d3721@gmail.com>
2017-07-22 22:21:06 +0800

    Detect page position for navbar

--- src/js/scroll_event.js
+++ src/js/scroll_event.js
@@ -1,5 +1,26 @@
-const bodyHeight = $(document.body).height();
+import fastdom from 'fastdom';
 
+
+/**** Detect page position for navbar ****/
 $(window).scroll(function(){
-    $('.gear').css({ transform: 'rotate('  + ($(document.body).scrollTop() / bodyHeight * 360 * 8) +'deg)' });
+
+    let active_btn = null;
+
+    fastdom.measure(() => {
+        let page_offset_top = $(window).scrollTop();
+        $('.selection').each((ith, elem) => {
+            const target_selector_id = '#' + $(elem).data('target');
+            const target_offset_top = $(target_selector_id).offset().top;
+            if( target_offset_top - 300 < page_offset_top )
+                active_btn = elem;
+        });
+    });
+
+    fastdom.mutate(() => {
+        $('.selection').removeClass('active');
+        if( active_btn )
+            $(active_btn).addClass('active');
+    });
+
 });
+/*****************************************/
