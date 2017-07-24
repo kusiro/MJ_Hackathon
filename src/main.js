4176bbec2fe8c4f436e36d72e1283c35c9e2f1d7
kusiro <jackysoon871008@gmail.com>
2017-07-24 22:28:14 +0800

    particles js

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ import ClickEvent from "./js/click_event.js";
 import ScrollEvent from './js/scroll_event.js';
 import ScrollReveal from "./js/scroll_reveal.js";
 import scrollSpeed from "./js/scroll-parallax.js";
+require('./js/particles.js');
+
+particlesJS.load('particles_container', './src/json/intro_partical.json');
+
+particlesJS.load('join_us', './src/json/join_us_partical.json');
