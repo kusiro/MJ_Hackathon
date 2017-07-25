a4fe151793ea28f8a554b56b1d677ebc0ab99c46
kusiro <jackysoon871008@gmail.com>
2017-07-25 18:03:50 +0800

    fix particle

--- src/main.js
+++ src/main.js
@@ -4,8 +4,4 @@ import ClickEvent from "./js/click_event.js";
 import ScrollEvent from './js/scroll_event.js';
 import ScrollReveal from "./js/scroll_reveal.js";
 import scrollSpeed from "./js/scroll-parallax.js";
-require('./js/particles.js');
-
-particlesJS.load('particles_container', '/intro_partical.json');
-
-particlesJS.load('join_us', '/src/json/join_us_partical.json');
+import particles from "./js/perfect_particles.js";
