033c194b55e918166993d5a2009c00ef4774bdc8
kusiro <jackysoon871008@gmail.com>
2017-08-03 12:56:42 +0800

    gallery

--- src/js/scroll-parallax.js
+++ src/js/scroll-parallax.js
@@ -12,10 +12,6 @@ if( my_width >= laptop ){
             element: $(".block_1_img")[0],
             distance: 6,
             offset: 100,
-        },{
-            element: $(".block_2_img")[0],
-            distance: 4,
-            offset: 500,
         },
     ]);
 }
